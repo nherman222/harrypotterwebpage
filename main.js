a1c63f1068b70382810ea9eaa29a784d2867da3a
Nina Herman <nina.n.herman.22@dartmouth.edu>
2020-05-16 12:33:11 -0400

    add preloader

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const playBtn = document.querySelector(".intro input");
+const wandBtn = document.querySelector(".intro input");
 const introScreen = document.querySelector(".intro");
 const match = document.querySelector(".match");
 
-playBtn.addEventListener("click", () => {
-  document.querySelector("html").style.background = "#06061c";
-  location.href = "about.html";
+wandBtn.addEventListener("click", () => {
+  document.querySelector(".intro").style.visibility = "hidden";
+  document.querySelector(".preload").style.visibility = "visible";
+  setTimeout("location.href = 'about.html';", 2500);
 });
